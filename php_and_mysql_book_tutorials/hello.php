20e6cb3117d35ac0a7071ad6a220979fd49d17cf
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2024-04-27 22:48:11 -0400

    Chapter 1 - Getting started

---
+++ php_and_mysql_book_tutorials/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Getting Started with PHP</title>
+</head>
+
+<body>
+    <?php 
+    # Write the traditional greeting.
+    echo '<h1>Hello world!</h1>';
+    ?>
+</body>
+
+</html>
---
+++ php_and_mysql_book_tutorials/require.php
@@ -0,0 +1,9 @@
+<?php
+# Incorporate the MySQL connection script.
+require ('../../connect_db.php');
+# Display MySQL version and host.
+if (mysqli_ping($dbc)) {
+    echo 'MySQL Server ' . mysqli_get_server_info($dbc) .
+        ' connected on ' . mysqli_get_host_info($dbc);
+}
+?>
