89e71796c73d8366326c2aaa1fadf1bb51427dcc
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2024-06-25 22:55:08 -0400

    Chapter 9 - Providing forums

---
+++ php_and_mysql_book_tutorials/sql_files/create_forum.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS forum (
+    post_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    subject VARCHAR(60) NOT NULL,
+    message TEXT NOT NULL,
+    post_date DATETIME NOT NULL,
+    PRIMARY KEY(post_id)
+);
